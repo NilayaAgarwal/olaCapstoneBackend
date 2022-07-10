0eadf91f7b7088c04e65b69a95945469600179e1
Nilaya Agarwal <nilaya.agarwal@olacabs.com>
2022-07-10 11:08:15 +0530

    policy updated

--- src/main/java/com/olacapstone/socialbackend/SocialbackendApplication.java
+++ src/main/java/com/olacapstone/socialbackend/SocialbackendApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
 
 @SpringBootApplication
 public class SocialbackendApplication {
@@ -13,15 +14,29 @@ public class SocialbackendApplication {
 		SpringApplication.run(SocialbackendApplication.class, args);
 	}
 
-	@Bean
-	public WebMvcConfigurer configure() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry reg) {
-				reg.addMapping("/**").allowedOrigins("*");
-			}
-		};
+	// @Bean
+	// public WebMvcConfigurer configure() {
+	// 	return new WebMvcConfigurer() {
+	// 		@Override
+	// 		public void addCorsMappings(CorsRegistry reg) {
+	// 			reg.addMapping("/**").allowedOrigins("*");
+	// 		}
+	// 	};
 
-	}
+	// }
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedMethods(CorsConfiguration.ALL)
+                        .allowedHeaders(CorsConfiguration.ALL)
+                        .allowedOriginPatterns(CorsConfiguration.ALL);
+            }
+        };
+    }
 
 }
